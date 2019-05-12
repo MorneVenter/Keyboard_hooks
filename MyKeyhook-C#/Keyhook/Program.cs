4e5eadc9b4b6712a43cf787eb6dd6618cb1ce345
Morne <morneventer.mv@gmail.com>
2019-05-12 14:56:56 +0200

    Init C# files

---
+++ MyKeyhook-C#/Keyhook/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Diagnostics;
+
+using System.Runtime.InteropServices;
+
+namespace Keyhook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
