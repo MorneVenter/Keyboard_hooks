4293b57a2cc3bb4fad3adecac3e94d74e5aa2200
Coenraad Human <coen.human@gmail.com>
2019-05-15 14:18:59 +0200

    User Friendly Improvements - GUI
    - Added status field.
    - Added output path.
    - Added shortcut key.

---
+++ C#-Based/MyKeyhook/Main.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace MyKeyhook
+{
+    partial class SuspiciousApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.labelShortcut = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.labelPath = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Location = new System.Drawing.Point(6, 16);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(46, 13);
+            this.labelStatus.TabIndex = 2;
+            this.labelStatus.Text = "Status...";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.labelStatus);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(305, 38);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Status";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.labelShortcut);
+            this.groupBox2.Location = new System.Drawing.Point(12, 56);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(305, 38);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Shortcut";
+            // 
+            // labelShortcut
+            // 
+            this.labelShortcut.AutoSize = true;
+            this.labelShortcut.Location = new System.Drawing.Point(6, 16);
+            this.labelShortcut.Name = "labelShortcut";
+            this.labelShortcut.Size = new System.Drawing.Size(43, 13);
+            this.labelShortcut.TabIndex = 0;
+            this.labelShortcut.Text = "F1 + F4";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.labelPath);
+            this.groupBox3.Location = new System.Drawing.Point(12, 100);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(305, 38);
+            this.groupBox3.TabIndex = 5;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Key Logger Output File";
+            // 
+            // labelPath
+            // 
+            this.labelPath.AutoSize = true;
+            this.labelPath.Location = new System.Drawing.Point(6, 16);
+            this.labelPath.Name = "labelPath";
+            this.labelPath.Size = new System.Drawing.Size(43, 13);
+            this.labelPath.TabIndex = 0;
+            this.labelPath.Text = "F1 + F4";
+            // 
+            // SuspiciousApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(329, 150);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(345, 189);
+            this.MinimumSize = new System.Drawing.Size(345, 189);
+            this.Name = "SuspiciousApp";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "C# Based Keyboard Hook";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label labelStatus;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label labelShortcut;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label labelPath;
+    }
+}
+
